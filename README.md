5e42c670100ca297d71d0f335c0bb5b4491d0514
Israr Ahmed <israrahmedpk1999@gmail.com>
2023-12-09 16:15:30 +0500

    final commit

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 API that is built using Node js used to fetch Football related news from all the top news websites of the world
 
-To install simply run npm install
+## `Follow these instructions to run the API successfully`
+
+## Step 1: Install dependencies
+
+Installation:
+
+npm install
+
+## Step 2: Start the backend server
+
+npm start or npm run dev
+
+Open your favorite browser and navigate to http://localhost:8080 to experience the magic 😎!
+
+The app will automatically reload as you make changes, and any lint errors will be displayed in the console.
+
+## Step 3: Testing in Postman
+
+Run the interactive test runner using:
+
+npm test
+
+Refer to the running tests documentation for more information.
+
+
+## `🚀 Note to Repo Visitors`
+If you find this repo useful, don't forget to show some love! Give the repository a star to support the project. 🌟
 
--- index.js
+++ index.js
@@ -1,82 +1,85 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const articles = []
+const articles = [];
 
 const app = express();
+
+//Newspapers that we will target for getting news
 const information = [
-    {
-        name: "BBC",
-        address: "https://www.bbc.com/sport/football",
-    },
-    {
-        name: "Gaurdian",
-        address: "https://www.theguardian.com/football",
-        // base:"https://www.theguardian.com"
-    },
-    {
-        name: "SkySports",
-        address: "https://www.skysports.com/football",
-    }
-]
+  {
+    name: "BBC",
+    address: "https://www.bbc.com/sport/football",
+  },
+  {
+    name: "Gaurdian",
+    address: "https://www.theguardian.com/football",
+  },
+  {
+    name: "SkySports",
+    address: "https://www.skysports.com/football",
+  },
+];
 
-information.forEach(info => {
-    axios.get(info.address)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
+//Parsing websites for getting data
+information.forEach((info) => {
+  axios.get(info.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-            $('a:contains("football")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr("href")
+    $('a:contains("football")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
 
-                articles.push({
-                    title,
-                    //url:information.base+url
-                    url,
-                    source: info.name,
-                })
-            })
+      articles.push({
+        title,
+        //url:information.base+url
+        url,
+        source: info.name,
+      });
+    });
+  });
+});
 
-        })
-})
-
-
+//Returned array of objects to be accessible by this route
 app.get("/news", (req, res) => {
-    res.send(articles)
-})
-
+  res.status(200).send(articles);
+});
 
 //For getting results for specific newspaper
-app.get('/news/:informationId', (req, res) => {
-    const informationId = req.params.informationId
+app.get("/news/:informationId", (req, res) => {
+  const informationId = req.params.informationId;
 
+  const informationAddress = information.filter(
+    (info) => info.name == informationId
+  )[0].address;
+  const informationBase = information.filter(
+    (info) => info.name == informationId
+  )[0].base;
 
-    const informationAddress = information.filter(info => info.name == informationId)[0].address
-    const informationBase = information.filter(info => info.name == informationId)[0].base
+  axios
+    .get(informationAddress)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const specificArticles = [];
 
+      $('a:contains("football")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr("href");
+        specificArticles.push({
+          title,
+          url: informationBase + url,
+          source: informationId,
+        });
+      });
+      res.json(specificArticles);
+    })
+    .catch((err) => console.log(err));
+});
 
-    axios.get(informationAddress)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const specificArticles = []
-
-            $('a:contains("football")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                specificArticles.push({
-                    title,
-                    url: informationBase + url,
-                    source: informationId
-                })
-            })
-            res.json(specificArticles)
-        }).catch(err => console.log(err))
-})
-
-
+//Server is listening at port 8080
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log("Hello from server")
-})
+  console.log("Hello from server");
+});
